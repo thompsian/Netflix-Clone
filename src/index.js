0cfbe0b1150c6a559820c5532018693735b9e4bd
thompsian <thompsian@gmail.com>
2021-10-08 13:31:35 -0700

    Global styles

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { GlobalStyles } from './global-styles';
 import { App } from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+        <>
+            <GlobalStyles />
+            <App />
+        </>
   </React.StrictMode>,
   document.getElementById('root')
 );
