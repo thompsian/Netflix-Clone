231f01e95dc523b839ff482ccadc63597b838056
thompsian <thompsian@gmail.com>
2021-10-09 13:48:26 -0700

    Opt-Form Component

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Accordion } from '../components';
+import { Accordion, OptForm } from '../components';
 import faqsData from '../fixtures/faqs.json';
 
 export function FaqsContainer() {
@@ -14,5 +14,9 @@ export function FaqsContainer() {
                 </Accordion.Item>
             ))}
         </Accordion.Frame>
+
+        <OptForm>
+            <OptForm.Input placeholder="Email Address" />
+        </OptForm>
     </Accordion>);
 }
