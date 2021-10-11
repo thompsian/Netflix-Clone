aeedffeff6f9d3b0e6926ea6cf0ee3e130881cf5
thompsian <thompsian@gmail.com>
2021-10-11 15:50:27 -0700

    feature component styling

--- src/pages/home.js
+++ src/pages/home.js
@@ -9,16 +9,16 @@ export default function Home() {
   return (
     <>
         <HeaderContainer>
-                <OptForm>
-                    <Feature>
-                        <Feature.Title>Unlimited films, TV programs and more.</Feature.Title>
-                        <Feature.SubTitle>Watch anywhere. Cancel at any time.</Feature.SubTitle>
-                        <OptForm.Input placeholder="Email Address" />
-                        <OptForm.Button>Try it now</OptForm.Button>
-                        <OptForm.Break />
-                        <OptForm.Text>Ready to watch? Enter your email to create or restart your membership.</OptForm.Text>
-                    </Feature>
+            <Feature>
+                <Feature.Title>Unlimited films, TV programs and more.</Feature.Title>
+                <Feature.SubTitle>Watch anywhere. Cancel at any time.</Feature.SubTitle>
+                <OptForm>    
+                    <OptForm.Input placeholder="Email Address" />
+                    <OptForm.Button>Try it now</OptForm.Button>
+                    <OptForm.Break />
+                    <OptForm.Text>Ready to watch? Enter your email to create or restart your membership.</OptForm.Text>
                 </OptForm>
+            </Feature>
         </HeaderContainer>
         <JumbotronContainer />
         <FaqsContainer />
