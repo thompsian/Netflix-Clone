f6f16ffd0e4a3cf15d0a9a6119a7b894c4d30308
thompsian <thompsian@gmail.com>
2021-10-11 15:44:24 -0700

    Add Feature Component

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { OptForm } from '../components';
+import { Feature, OptForm } from '../components';
 import { FaqsContainer } from '../containers/faqs';
 import { JumbotronContainer } from '../containers/jumbotron';
 import { FooterContainer } from '../containers/footer';
@@ -9,12 +9,16 @@ export default function Home() {
   return (
     <>
         <HeaderContainer>
-            <OptForm>
-                <OptForm.Input placeholder="Email Address" />
-                <OptForm.Button>Try it now</OptForm.Button>
-                <OptForm.Break />
-                <OptForm.Text>Ready to watch? Enter your email to create or restart your membership.</OptForm.Text>               
-            </OptForm>
+                <OptForm>
+                    <Feature>
+                        <Feature.Title>Unlimited films, TV programs and more.</Feature.Title>
+                        <Feature.SubTitle>Watch anywhere. Cancel at any time.</Feature.SubTitle>
+                        <OptForm.Input placeholder="Email Address" />
+                        <OptForm.Button>Try it now</OptForm.Button>
+                        <OptForm.Break />
+                        <OptForm.Text>Ready to watch? Enter your email to create or restart your membership.</OptForm.Text>
+                    </Feature>
+                </OptForm>
         </HeaderContainer>
         <JumbotronContainer />
         <FaqsContainer />
