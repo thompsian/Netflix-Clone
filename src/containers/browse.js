4d2da23511baa64b18e50359eff281299445c2a2
thompsian <thompsian@gmail.com>
2021-10-14 15:10:29 -0700

    added dropdown to header component

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -10,7 +10,7 @@ export function BrowseContainer() {
     const [profile, setProfile] = useState({});
     const [loading, setLoading] = useState(true);
     const [searchTerm, setSearchTerm] = useState('');
-    
+
     const { firebase } = useContext(FirebaseContext);
 
     const user = {
@@ -37,6 +37,18 @@ export function BrowseContainer() {
                     </Header.Group>
                     <Header.Group>
                         <Header.Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+                        <Header.Profile>
+                            <Header.Picture src={user.photoURL} />
+                            <Header.Dropdown>
+                                <Header.Group>
+                                    <Header.Picture src={user.photoURL} />
+                                    <Header.Link>{user.displayName}</Header.Link>
+                                </Header.Group>
+                                <Header.Group>
+                                    <Header.Link onClick={() => firebase.auth().signOut()}>Sign out</Header.Link>
+                                </Header.Group>
+                            </Header.Dropdown>
+                        </Header.Profile>
                     </Header.Group>
                 </Header.Frame>
 
