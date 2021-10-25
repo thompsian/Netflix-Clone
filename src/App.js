5455d6bab3c8c54f6b1e760b30de80dc082f1575
thompsian <thompsian@gmail.com>
2021-10-25 11:49:10 -0700

    implement auth listener hook

--- src/App.js
+++ src/App.js
@@ -3,24 +3,27 @@ import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 import { Home, Browse, Signin, Signup } from './pages';
+import { useAuthListener } from './hooks';
 
 export function App() {
-  return (
-    <Router>
-        <Switch>
-            <Route path={ROUTES.SIGN_IN}>
-                <Signin />
-            </Route>
-            <Route path={ROUTES.SIGN_UP}>
-                <Signup />
-            </Route>
-            <Route path={ROUTES.BROWSE}>
-                <Browse />
-            </Route>
-            <Route path={ROUTES.HOME}>
-                <Home />
-            </Route>
-        </Switch>
-    </Router>
-  );
+    const { user } = useAuthListener();
+
+    return (
+        <Router>
+            <Switch>
+                <Route path={ROUTES.SIGN_IN}>
+                    <Signin />
+                </Route>
+                <Route path={ROUTES.SIGN_UP}>
+                    <Signup />
+                </Route>
+                <Route path={ROUTES.BROWSE}>
+                    <Browse />
+                </Route>
+                <Route path={ROUTES.HOME}>
+                    <Home />
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
